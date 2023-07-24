4af076a07374188f127a37b2bf1e9a7117cf0a55
Kirill <kirillmensikov90@gmail.com>
2023-07-24 18:18:18 +0300

    1

--- advertisment/app_advertisment/urls.py
+++ advertisment/app_advertisment/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, advertisement_post, register, login, profile, advertisement
 
 urlpatterns = [
-    path('', index)
+    path('index.html/', index, name="main-page"),
+    path('top-sellers/', top_sellers, name="top-sellers"),
+    path('advertisement-post.html/', advertisement_post, name="advertisement-post"),
+    path('register.html/', register, name="register"),
+    path('login.html/', login, name="login"),
+    path('profile.html/', profile, name="profile"),
+    path('advertisement.html/', advertisement, name="advertisement"),
 ]
